61c72fb7f52b55c26d830eb4f8792fb1dd6b6175
igorrm19 <git@stackblitz.com>
2024-09-29 03:42:00 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <h1>Igor RM</h1>
+    </> 
+  )
+}
+
+export default App
---
+++ src/Appes.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import App from './App.tsx';
+
+function Appes() {
+  return (
+    <>
+      <h2>gostoso</h2>
+      <App />
+    </>
+  );
+}
+
+export default Appes;
